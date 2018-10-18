62470c2b9876b39be057cfee65dbb53d8e14d47c
Steven Graham <1858109+scragraham@users.noreply.github.com>
2018-10-18 00:17:04 -0500

    initial license and setup files for appscale-agents

---
+++ README.md
@@ -0,0 +1,21 @@
+# AppScale Agents
+
+A set of cloud agents that are used by appscale tools
+
+## Installing AppScale Agents
+See [Installing the AppScale Agents](https://github.com/AppScale/appscale-agents/wiki/Installing-the-AppScale-Agents).
+
+## Installing from Source
+To install the very latest code on your system:
+
+1. Make sure [setuptools](https://pypi.python.org/pypi/setuptools) is installed.
+2. Run `# pip install .` from this directory.
+
+## What is AppScale?
+AppScale is an open-source cloud computing platform that automatically deploys and scales unmodified Google App Engine
+applications over public and private cloud systems and on-premise clusters. AppScale is modeled on the App Engine APIs
+and has support for Python, Go, PHP and Java applications.
+
+AppScale is developed and maintained by AppScale Systems, Inc., based in Santa Barbara, California, and Google.
+
+See http://github.com/AppScale/appscale for the main branch.
---
+++ setup.py
@@ -0,0 +1,83 @@
+import sys
+
+from setuptools import setup
+
+# Require users to uninstall versions that used the appscale namespace.
+try:
+  import appscale.appscale_tools
+  print('Please run "pip uninstall appscale-tools" first.\n'
+        "Your installed version conflicts with this version's namespace.")
+  sys.exit()
+except ImportError:
+  pass
+
+
+long_description = """AppScale Agents 
+--------------
+
+A set of agents for interacting with various clouds 
+
+What is AppScale?
+-----------------
+
+AppScale is an open-source cloud computing platform that automatically deploys
+and scales unmodified Google App Engine applications over public and private
+cloud systems and on-premise clusters. AppScale is modeled on the App Engine
+APIs and has support for Python, Go, PHP and Java applications.
+
+AppScale is developed and maintained by AppScale Systems, Inc., based in
+Santa Barbara, California, and Google.
+
+http://www.appscale.com
+"""
+
+setup(
+  name='appscale-agents',
+  version='3.6.1',
+  description='A set of agents for interacting with various clouds',
+  long_description=long_description,
+  author='AppScale Systems, Inc.',
+  url='https://github.com/scragraham/appscale-agents',
+  license='Apache License 2.0',
+  keywords='appscale google-app-engine python java go php',
+  platforms='Posix; MacOS X',
+  install_requires=[
+    'adal>=0.4.7',
+    'azure==2.0.0',
+    'azure-mgmt-marketplaceordering',
+    'cryptography',
+    'argparse',
+    'boto',
+    'google-api-python-client==1.5.4',
+    'haikunator',
+    'httplib2',
+    'msrestazure==0.4.34',
+    'oauth2client==4.0.0',
+    'PyYAML',
+    'requests[security]>=2.16.0,<=2.19.1',
+    'retrying==1.3.3',
+    'setuptools>=11.3,<34',
+    'SOAPpy',
+    'tabulate==0.7.7',
+    'termcolor',
+    'wstools==0.4.3'
+  ],
+  extras_require={'testing': ['mock']},
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Utilities'
+  ],
+  namespace_packages=['appscale'],
+  packages=['appscale', 'appscale.agents'],
+  entry_points={
+    'console_scripts': [
+    ]
+  },
+  package_data={}
+)
