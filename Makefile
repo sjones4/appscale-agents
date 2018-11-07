404aaa170484bec4de544d9c12d48688a48902f7
Steven Graham <1858109+scragraham@users.noreply.github.com>
2018-11-07 17:15:32 -0600

    - added .gitignore with typical settings. - modified Makefile, added, clean/build/upload. - fixed test_upgrade_json to properly pass the test.

--- Makefile
+++ Makefile
@@ -14,13 +14,28 @@ test: checkenv-VIRTUAL_ENV ## Run Python Unit tests
 	@pip install flexmock
 	@python -m unittest discover -b -v -s test
 
-##@ Build / Installation
+##@ Installation
 
 install: checkenv-VIRTUAL_ENV ## Install via pip, ensuring a virtualenv
 	@pip install .
 
 install-no-venv: ## Install via pip without ensuring a virtualenv
 	@pip install .
+
+##@ Build (for PyPi)
+
+build: checkenv-VIRTUAL_ENV ## Build distro for pypi upload
+	@python setup.py sdist bdist_wheel
+
+
+clean: ## Clean build directories
+	rm -rf build
+	rm -rf dist
+	rm -rf appscale_agents.egg-info
+
+upload: checkenv-VIRTUAL_ENV ## Upload distribution to pypi
+	@pip install --user --upgrade twine
+	$(info "Uploading to twine")
 
 ##@ Utilities
 
--- test/test_config_appscale_state.py
+++ test/test_config_appscale_state.py
@@ -85,7 +85,7 @@ class TestAppScaleState(TestCase):
     ]
 }
         """
-        self.old_confg_yaml = """
+        self.old_config_yaml = """
         {
         "infrastructure_info": {
             "group": "sgrahamappgroup",
@@ -322,54 +322,16 @@ class TestAppScaleState(TestCase):
         json_loc = AppScaleState.locations_json_location(self.default_keyname)
         yaml_loc = AppScaleState.locations_yaml_location(self.default_keyname)
 
-        with open(json_loc, 'w') as json_fh:
+        with open(json_loc, 'wc') as json_fh:
             json_fh.write(self.old_config_json)
 
-        with open(yaml_loc, 'w') as yaml_fh:
-            yaml_fh.write(self.old_confg_yaml)
+        with open(yaml_loc, 'wc') as yaml_fh:
+            yaml_fh.write(self.old_config_yaml)
 
         AppScaleState.upgrade_json_file(self.default_keyname)
 
         # JSON content should be the
-        expected = """
-        {
-        "infrastructure_info": {
-            "group": "sgrahamappgroup",
-            "infrastructure": "gce",
-            "project": "appscale-staging",
-            "zone": "us-central1-a",
-            "azure_storage_account":"UNIT_TEST_STORAGE",
-            "azure_resource_group":"UNIT_TEST_RESOURCE",
-            "azure_tenant_id":"UNIT_TEST_TENANT_ID",
-            "azure_app_secret_key":"SSSSHITSASECRET",
-            "azure_app_id":"UNIT_TEST_APPID",
-            "azure_subscription_id":"UNIT_TEST_SUBSCRIPTION_ID",
-        },
-        "node_info": [
-        {
-            "cloud": "cloud1",
-            "disk": null,
-            "instance_id": "dummyappgroup-9c3dc2be-a1ca-4dca-a32a-3dd58c407031",
-            "instance_type": "n1-standard-1",
-            "jobs": [
-                "load_balancer",
-                "compute",
-                "database",
-                "zookeeper",
-                "taskqueue_master",
-                "db_master",
-                "taskqueue",
-                "memcache",
-                "shadow",
-                "login"
-            ],
-            "private_ip": "10.240.0.2",
-            "public_ip": "35.232.41.161",
-            "ssh_key": "/etc/appscale/keys/cloud1/appscale3cc1f78769994c6ab909d278ff18d0e3.key"
-        }
-    ]
-}
-        """
+        expected = """{"node_info": {"node_info": [{"public_ip": "35.232.41.161", "jobs": ["load_balancer", "compute", "database", "zookeeper", "taskqueue_master", "db_master", "taskqueue", "memcache", "shadow", "login"], "ssh_key": "/etc/appscale/keys/cloud1/appscale3cc1f78769994c6ab909d278ff18d0e3.key", "instance_id": "dummyappgroup-9c3dc2be-a1ca-4dca-a32a-3dd58c407031", "instance_type": "n1-standard-1", "private_ip": "10.240.0.2", "disk": null, "cloud": "cloud1"}]}, "infrastructure_info": {"infrastructure_info": {"azure_app_secret_key": "SSSSHITSASECRET", "infrastructure": "gce", "group": "sgrahamappgroup", "zone": "us-central1-a", "project": "appscale-staging", "azure_resource_group": "UNIT_TEST_RESOURCE", "azure_tenant_id": "UNIT_TEST_TENANT_ID", "azure_subscription_id": "UNIT_TEST_SUBSCRIPTION_ID", "azure_storage_account": "UNIT_TEST_STORAGE", "azure_app_id": "UNIT_TEST_APPID"}}}"""
         actual = "DIDNTREAD"
         with open(json_loc, 'r') as json_fh:
             actual = json_fh.read()
